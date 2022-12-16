eebd1f3f58a7d1f44178cbf17e49aef7fb8777fb
Hafsa4321 <120473600+Hafsa4321@users.noreply.github.com>
2022-12-16 04:11:46 -0800

    Add files via upload

---
+++ Hafsa.project.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+fillcolor('purple')
+
+begin_fill()
+
+sides=5
+distance = 50
+
+for _ in range(50*sides):
+   distance+=20
+   forward(distance)
+   right(2*360.0/sides+1)
+
+   end_fill()
+   
